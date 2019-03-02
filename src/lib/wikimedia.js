f6582aafa74eab2ff924a07981d85c509f1345b7
Jasper <well.caffeinated@gmail.com>
2019-03-02 12:04:00 -0500

    filter out weird gorilla poster

--- src/lib/wikimedia.js
+++ src/lib/wikimedia.js
@@ -43,7 +43,7 @@ export function findImagesByName( name, { limit, thumbSize } = {} ){
     'action': 'query'
     , 'prop': 'imageinfo'
     , 'generator': 'search'
-    , 'gsrsearch': `File:${name} NOT map NOT atlas NOT lineage NOT svg NOT phylogram NOT cladogram`
+    , 'gsrsearch': `File:${name} NOT map NOT atlas NOT lineage NOT svg NOT phylogram NOT cladogram NOT Destroy_this_mad_brute`
     , 'format': 'json'
     , 'origin': '*'
     , 'iiprop': 'url|size'
